9ada7fed3e534289fc2d9de9cc3da858f3400866
DESKTOP-GJDT5D1\Tummy <vudtipat@gmail.com>
2023-04-13 12:55:28 +0700

    add assets and add header footer wrapper component

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,7 @@
 import '@/styles/globals.css'
 import Head from "next/head";
+import Header from '@/components/Header';
+import Footer from '@/components/Footer';
 
 export default function App({ Component, pageProps }) {
   return (
@@ -26,7 +28,9 @@ export default function App({ Component, pageProps }) {
             rel="stylesheet"
         />
       </Head>
+      <Header/>
       <Component {...pageProps} />
+      <Footer/>
     </>
   );
 }
