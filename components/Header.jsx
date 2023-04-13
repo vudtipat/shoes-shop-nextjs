a5e8e0b4330bee4ebff318300008d8caa2f07533
DESKTOP-GJDT5D1\Tummy <vudtipat@gmail.com>
2023-04-13 15:21:58 +0700

    add menu and update header tab

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,6 +1,7 @@
 import React,{useState,useEffect} from "react";
 import Wrapper from "./Wrapper";
 import Link from "next/link";
+import Menu from "./Menu";
 
 const Header = () => {
     const [mobileMenu, setMobileMenu] = useState(false);
@@ -12,10 +13,12 @@ const Header = () => {
     return (
     <header className={`w-full h-[50px] md:h-[80px] bg-white flex items-center justify-between 
         z-20 sticky top-0 transition-transform duration-300 ${show}`}>
-        <Wrapper>
+        <Wrapper className="h-[60px] flex justify-between items-center">
             <Link href="/">
                 <img src='/logo.svg' className="w-[40px] md:w-[60px]"/>
             </Link>
+
+            <Menu showCatMenu={showCatMenu} setShowCatMenu={setShowCatMenu}/>
         </Wrapper>
     </header>);
 }
