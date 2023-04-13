203bf594bbb7391596b573e2101bce4fcc8fedae
DESKTOP-GJDT5D1\Tummy <vudtipat@gmail.com>
2023-04-13 21:44:24 +0700

    edit header

--- components/Header.jsx
+++ components/Header.jsx
@@ -3,6 +3,11 @@ import Wrapper from "./Wrapper";
 import Link from "next/link";
 import Menu from "./Menu";
 
+import { IoMdHeartEmpty } from "react-icons/io";
+import { BsCart } from "react-icons/bs";
+import { BiMenuAltRight } from "react-icons/bi";
+import { VscChromeClose } from "react-icons/vsc";
+
 const Header = () => {
     const [mobileMenu, setMobileMenu] = useState(false);
     const [showCatMenu, setShowCatMenu] = useState(false);
@@ -10,6 +15,7 @@ const Header = () => {
     const [lastScrollY, setLastScrollY] = useState(0);
     const [categories, setCategories] = useState(null);
 
+    
     return (
     <header className={`w-full h-[50px] md:h-[80px] bg-white flex items-center justify-between 
         z-20 sticky top-0 transition-transform duration-300 ${show}`}>
@@ -19,6 +25,42 @@ const Header = () => {
             </Link>
 
             <Menu showCatMenu={showCatMenu} setShowCatMenu={setShowCatMenu}/>
+        
+            <div className="flex items-center gap-2 text-black">
+                {/* Icon Start */}
+                <div className="w-8 md:w-12 h-8 md:h-12 rounded-full flex justify-center items-center hover:bg-black/[0.05] cursor-pointer relative">
+                    <IoMdHeartEmpty className="text-[15px] md:text-[20px]"/>
+                    <div className="h-[14px] md:h-[18px] min-w-[14px] md:mix-w-[18px] rounded-full bg-red-600 absolute top-1 left-5 md:left-7 text-white text-[10px] md:text-[12px] flex justify-center items-center px-[2px] md:px-[5px]">
+                        3
+                    </div>
+                </div>
+                {/* Icon end */}
+
+                {/* Icon Start */}
+                <div className="w-8 md:w-12 h-8 md:h-12 rounded-full flex justify-center items-center hover:bg-black/[0.05] cursor-pointer relative">
+                    <BsCart className="text-[15px] md:text-[20px]"/>
+                    <div className="h-[14px] md:h-[18px] min-w-[14px] md:mix-w-[18px] rounded-full bg-red-600 absolute top-1 left-5 md:left-7 text-white text-[10px] md:text-[12px] flex justify-center items-center px-[2px] md:px-[5px]">
+                        5
+                    </div>
+                </div>
+                {/* Icon end */}
+
+                {/* Mobile Icon Start*/}
+                <div className="w-8 md:w-12 h-8 md:h-12 rounded-full flex justify-center items-center hover:bg-black/[0.05] cursor-pointer relative -mr-2">
+                    {mobileMenu ? (
+                        <VscChromeClose
+                            className="text-[16px]"
+                            onClick={() => setMobileMenu(false)}
+                        />
+                    ): (
+                        <BiMenuAltRight
+                            className="text-[20px]"
+                            onClick={() => setMobileMenu(true)}
+                        />
+                    )}
+                </div>
+                {/* Mobile Icon End*/}
+            </div>
         </Wrapper>
     </header>);
 }
