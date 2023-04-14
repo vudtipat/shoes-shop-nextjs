5dabc52852ad01b9c07d78726f2145b623e65691
DESKTOP-GJDT5D1\Tummy <vudtipat@gmail.com>
2023-04-14 14:11:37 +0700

    add product card

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import HeroBanner from '@/components/HeroBanner'
 import Wrapper from '@/components/Wrapper'
+import ProductCard from '@/components/ProductCard'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -9,6 +10,7 @@ export default function Home() {
   return (
     <main>
       <HeroBanner/>
+
       <Wrapper >
         <div className='text-center max-w-[800px] mx-auto my-[50px] md:my-[80px]'>
           <div className="text-[28px] md:text-[34px] mb-5 font-semibold leading-tight">
@@ -20,7 +22,21 @@ export default function Home() {
               during extended stretches of running.
           </div>
         </div>
+
+        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 my-14 px-5 md:px-0'>
+          <ProductCard/>
+          <ProductCard/>
+          <ProductCard/>
+          <ProductCard/>
+          <ProductCard/>
+          <ProductCard/>
+          <ProductCard/>
+          <ProductCard/>
+        </div>
+
       </Wrapper>
+
+      
     </main>
   )
 }
